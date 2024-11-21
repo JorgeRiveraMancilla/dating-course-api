13152d706020de47addb974a63a3bbe35f584594
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-21 17:44:14 -0300

    feat: implement log user activity

---
+++ Src/Helpers/LogUserActivity.cs
@@ -0,0 +1,31 @@
+using dating_course_api.Src.Data;
+using dating_course_api.Src.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace dating_course_api.Src.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next
+        )
+        {
+            var resultContext = await next();
+
+            if (context.HttpContext.User.Identity?.IsAuthenticated != true)
+                return;
+
+            var userId = resultContext.HttpContext.User.GetUserId();
+            var dataContext =
+                resultContext.HttpContext.RequestServices.GetRequiredService<DataContext>();
+            var user = await dataContext.Users.FindAsync(userId);
+
+            if (user is null)
+                return;
+
+            user.LastActive = DateTime.UtcNow;
+            await dataContext.SaveChangesAsync();
+        }
+    }
+}
