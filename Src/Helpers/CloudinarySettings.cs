08fd43c31a5a0d39a9d9d346817906cde0872c31
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-20 23:07:17 -0300

    feat: define cloudinary settings

---
+++ Src/Helpers/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+namespace dating_course_api.Src.Helpers
+{
+    public class CloudinarySettings
+    {
+        public required string CloudName { get; set; }
+        public required string ApiKey { get; set; }
+        public required string ApiSecret { get; set; }
+    }
+}
