9dbdc2854ea7b5092434b9d00c748cd2ad71034b
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-21 17:51:57 -0300

    feat: implement toggle like

---
+++ Src/Controllers/LikeController.cs
@@ -0,0 +1,44 @@
+using dating_course_api.Src.DTOs.Like;
+using dating_course_api.Src.Extensions;
+using dating_course_api.Src.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dating_course_api.Src.Controllers
+{
+    public class LikeController(IUnitOfWork unitOfWork) : BaseApiController
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        [HttpPost("{targetUserId:int}")]
+        public async Task<ActionResult> ToggleLike([FromRoute] int targetUserId)
+        {
+            var sourceUserId = User.GetUserId();
+
+            if (sourceUserId == targetUserId)
+                return BadRequest("You cannot like yourself");
+
+            var existingLike = await _unitOfWork.LikeRepository.GetUserLikeAsync(
+                sourceUserId,
+                targetUserId
+            );
+
+            if (existingLike is null)
+            {
+                var createLikeDto = new CreateLikeDto
+                {
+                    SourceUserId = sourceUserId,
+                    TargetUserId = targetUserId
+                };
+
+                await _unitOfWork.LikeRepository.CreateLikeAsync(createLikeDto);
+            }
+            else
+                await _unitOfWork.LikeRepository.DeleteLikeAsync(existingLike);
+
+            if (await _unitOfWork.Complete())
+                return Ok();
+
+            return BadRequest("Failed to update like");
+        }
+    }
+}
--- Src/DTOs/Like/CreateLikeDto.cs
+++ Src/DTOs/Like/CreateLikeDto.cs
@@ -2,6 +2,7 @@ namespace dating_course_api.Src.DTOs.Like
 {
     public class CreateLikeDto
     {
-        
+        public required int SourceUserId { get; set; }
+        public required int TargetUserId { get; set; }
     }
 }
