f64b0d0bb134a91295e4ae8ab0d636482d8d8af1
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-20 23:05:50 -0300

    feat: implement user repository

---
+++ Src/Data/Repositories/UserRepository.cs
@@ -0,0 +1,118 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using dating_course_api.Src.DTOs.Member;
+using dating_course_api.Src.DTOs.User;
+using dating_course_api.Src.Entities;
+using dating_course_api.Src.Helpers.Pagination;
+using dating_course_api.Src.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace dating_course_api.Src.Data.Repositories
+{
+    public class UserRepository(
+        DataContext dataContext,
+        UserManager<User> userManager,
+        IMapper mapper
+    ) : IUserRepository
+    {
+        private readonly DataContext _dataContext = dataContext;
+        private readonly UserManager<User> _userManager = userManager;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<MemberDto?> GetMemberByEmailAsync(string email, bool isCurrentUser)
+        {
+            var query = _dataContext
+                .Users.Where(u => u.Email == email)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .AsQueryable();
+
+            if (isCurrentUser)
+                query = query.IgnoreQueryFilters();
+
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task<MemberDto?> GetMemberByIdAsync(int id, bool isCurrentUser)
+        {
+            var query = _dataContext
+                .Users.Where(u => u.Id == id)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .AsQueryable();
+
+            if (isCurrentUser)
+                query = query.IgnoreQueryFilters();
+
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
+        {
+            var query = _dataContext
+                .Users.AsQueryable()
+                .Where(u => u.Id != userParams.CurrentUserId);
+
+            if (userParams.Gender is not null)
+                query = query.Where(x => x.Gender == userParams.Gender);
+
+            var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+            var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+            query = query.Where(x => minDob <= x.BirthDate && x.BirthDate <= maxDob);
+
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(x => x.Created),
+                _ => query.OrderByDescending(x => x.LastActive)
+            };
+
+            return await PagedList<MemberDto>.CreateAsync(
+                query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider),
+                userParams.PageNumber,
+                userParams.PageSize
+            );
+        }
+
+        public async Task<UserDto?> GetUserByEmailAsync(string email)
+        {
+            return await _dataContext
+                .Users.Include(u => u.Photos)
+                .Where(u => u.Email == email)
+                .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<UserDto?> GetUserByIdAsync(int id)
+        {
+            return await _dataContext
+                .Users.Include(u => u.Photos)
+                .Where(u => u.Id == id)
+                .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<UserDto?> GetUserByPhotoIdAsync(int photoId)
+        {
+            return await _dataContext
+                .Users.Include(u => u.Photos)
+                .IgnoreQueryFilters()
+                .Where(u => u.Photos.Any(p => p.Id == photoId))
+                .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<UserDto>> GetUsersAsync()
+        {
+            return await _dataContext
+                .Users.Include(u => u.Photos)
+                .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
+        public void Update(UpdateUserDto updateUserDto)
+        {
+            var user = _mapper.Map<User>(updateUserDto);
+            _dataContext.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Src/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+using dating_course_api.Src.DTOs.Member;
+using dating_course_api.Src.DTOs.User;
+using dating_course_api.Src.Helpers.Pagination;
+
+namespace dating_course_api.Src.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<MemberDto?> GetMemberByEmailAsync(string email, bool isCurrentUser);
+        Task<MemberDto?> GetMemberByIdAsync(int id, bool isCurrentUser);
+        Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
+        Task<UserDto?> GetUserByEmailAsync(string email);
+        Task<UserDto?> GetUserByIdAsync(int id);
+        Task<UserDto?> GetUserByPhotoIdAsync(int photoId);
+        Task<IEnumerable<UserDto>> GetUsersAsync();
+        void Update(UpdateUserDto updateUserDto);
+    }
+}
