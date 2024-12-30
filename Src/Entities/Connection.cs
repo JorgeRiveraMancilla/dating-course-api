51c51fb271d9202c94982e875104d07b473af5ec
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-30 11:09:59 -0300

    feat: add connection tracking with status and timestamp

--- Src/Entities/Connection.cs
+++ Src/Entities/Connection.cs
@@ -4,5 +4,7 @@ namespace dating_course_api.Src.Entities
     {
         public required string ConnectionId { get; set; }
         public required int UserId { get; set; }
+        public required bool Connected { get; set; }
+        public DateTime ConnectedAt { get; set; } = DateTime.UtcNow;
     }
 }
